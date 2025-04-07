77d3c40b278a147315e73f87339e0a633906c7b6
daniilrusanov <daniil12099@gmail.com>
2025-04-07 12:15:39 +0300

    init skeleton

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kpp_module',
+  webDir: 'www'
+};
+
+export default config;
